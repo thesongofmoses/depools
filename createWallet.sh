5ccff333269a02fe242d40dde5fcaa6079dabfbe
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-18 10:11:12 +0000

    Update createWallet.sh

--- createWallet.sh
+++ createWallet.sh
@@ -9,7 +9,7 @@ rm "$hostname.seed.csv"
 for i in {1..4};
 do
 seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//')
-       echo "${seed_phrase}" >> "$hostname.seed.csv"
-parsed_seed_phrase=$(cat "$hostname.seed.csv" | awk "FNR == ${i}" | tr -d '"')
-        ./tonos-cli getkeypair "$hostname.${i}.keys.json" "$parsed_seed_phrase"
+       echo "${seed_phrase}" >> ~/ton-keys/"$hostname.seed.csv"
+parsed_seed_phrase=$(cat ~/ton-keys/"$hostname.seed.csv" | awk "FNR == ${i}" | tr -d '"')
+        ./tonos-cli getkeypair ~/ton-keys/"$hostname.${i}.keys.json" "$parsed_seed_phrase"
 done
