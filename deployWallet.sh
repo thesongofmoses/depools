fb34cdb0957d651f19892c19c18a5ca0f9c9f196
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-18 23:37:14 +0000

    Update deployWallet.sh

--- deployWallet.sh
+++ deployWallet.sh
@@ -13,3 +13,33 @@ cd ~/net.ton.dev/tonos-cli/target/release
         --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
         --sign ~/ton-keys/$hostname.1.keys.json \
         --wc 0
+
+deploy_addr=$(cat ~/ton-keys/$hostname.addr)
+proxy0_addr=$(cat ~/ton-keys/$hostname.proxy0.addr)
+proxy1_addr=$(cat ~/ton-keys/$hostname.proxy1.addr)
+depool_addr=$(cat ~/ton-keys/$hostname.depool.addr)
+helper_addr=$(cat ~/ton-keys/$hostname.helper.addr)
+
+./tonos-cli call $deploy_addr \
+        submitTransaction "{"\"dest"\":"\"${proxy0_addr}"\","\"value"\":200000000,"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
+        --sign ~/ton-keys/$hostname.1.keys.json \
+        | awk 'FNR == 18 {prtint $2}' | tr -d '"' > deploy.confirm.txid
+
+./tonos-cli call $deploy_addr \
+        submitTransaction "{"\"dest"\":"\"${proxy1_addr}"\","\"value"\":200000000,"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
+        --sign ~/ton-keys/$hostname.1.keys.json \
+        | awk 'FNR == 18 {prtint $2}' | tr -d '"' >> deploy.confirm.txid
+
+./tonos-cli call $deploy_addr \
+        submitTransaction "{"\"dest"\":"\"${depool_addr}"\","\"value"\":200000000,"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
+        --sign ~/ton-keys/$hostname.1.keys.json \
+        | awk 'FNR == 18 {prtint $2}' | tr -d '"' >> deploy.confirm.txid
+
+./tonos-cli call $deploy_addr \
+        submitTransaction "{"\"dest"\":"\"${helper_addr}"\","\"value"\":200000000,"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
+        --sign ~/ton-keys/$hostname.1.keys.json \
+        | awk 'FNR == 18 {prtint $2}' | tr -d '"' >> deploy.confirm.txid
