98cebc6ac0924c0751e83271101b3ddc5707b4b8
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-18 10:51:18 +0000

    Update createWallet.sh

--- createWallet.sh
+++ createWallet.sh
@@ -13,3 +13,7 @@ seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//')
 parsed_seed_phrase=$(cat ~/ton-keys/"$hostname.seed.csv" | awk "FNR == ${i}" | tr -d '"')
         ./tonos-cli getkeypair ~/ton-keys/"$hostname.${i}.keys.json" "$parsed_seed_phrase"
 done
+
+deploy_key=$(cat ~/ton-keys/"$hostname.1.keys.json") 
+raw_addr=$(./tonos-cli genaddr SafeMultisigWallet.tvc SafeMultisigWallet.abi.json --genkey $deploy_key --wc -1)
+        echo $raw_addr | awk 'FNR == 11 {print $3}' > ~/ton-keys/"$hostname.addr"
