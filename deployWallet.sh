c45b30f734926de8e47df31a5075d3ad0fe10627
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-18 22:50:52 +0000

    Update deployWallet.sh

--- deployWallet.sh
+++ deployWallet.sh
@@ -3,23 +3,13 @@
 username=$(whoami)
 hostname=$(hostname -s)
 
+pubkey1=$(echo 0x$(cat ~/ton-keys/$hostname.1.keys.json | grep 'public' | awk '{print $2}' | tr -d '"'\,))
+pubkey2=$(echo 0x$(cat ~/ton-keys/$hostname.2.keys.json | grep 'public' | awk '{print $2}' | tr -d '"'\,))
+pubkey3=$(echo 0x$(cat ~/ton-keys/$hostname.3.keys.json | grep 'public' | awk '{print $2}' | tr -d '"'\,))
+
 cd ~/net.ton.dev/tonos-cli/target/release
-rm ~/ton-keys/$hostname.custodian.seed.csv
-
-for i in {1..3};
-do
-seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//')
-       echo "${seed_phrase}" >> ~/ton-keys/$hostname.custodian.seed.csv
-done
-
-seed1=$(cat ~/ton-keys/$hostname.custodian.seed.csv | awk "FNR == 1" | tr -d '"')
-seed2=$(cat ~/ton-keys/$hostname.custodian.seed.csv | awk "FNR == 2" | tr -d '"')
-seed3=$(cat ~/ton-keys/$hostname.custodian.seed.csv | awk "FNR == 3" | tr -d '"')
-
-echo 0x$(./tonos-cli genpubkey "$seed1" | awk 'FNR == 3 {print $3}') > ~/ton-keys/$hostname.custodian.pubkey.csv
-echo 0x$(./tonos-cli genpubkey "$seed2" | awk 'FNR == 3 {print $3}') >> ~/ton-keys/$hostname.custodian.pubkey.csv
-echo 0x$(./tonos-cli genpubkey "$seed3" | awk 'FNR == 3 {print $3}') >> ~/ton-keys/$hostname.custodian.pubkey.csv
-
-pubkey1=$(sed -n 1p ~/ton-keys/$hostname.custodian.pubkey.csv)
-pubkey2=$(sed -n 2p ~/ton-keys/$hostname.custodian.pubkey.csv)
-pubkey3=$(sed -n 3p ~/ton-keys/$hostname.custodian.pubkey.csv)
+./tonos-cli deploy ~/net.ton.dev/configs/SafeMultisigWallet.tvc \
+        "{"\"owners"\":["\"${pubkey1}"\","\"${pubkey2}"\","\"${pubkey3}"\"],"\"reqConfirms"\":2}" \
+        --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
+        --sign ~/ton-keys/$hostname.1.keys.json \
+        --wc 0
