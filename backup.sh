e5f9ed3c827e7f704e709b71ee5aca3054c622c9
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-19 17:12:24 +0000

    Delete backup.sh

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..1};
-do
-ip=$(cat ~/backup/ip.csv | awk "FNR == ${i}")
-scp ${i}@$ip:~/ton-keys/*.keys.json ~/backup
-scp ${i}@$ip:~/ton-keys/*.addr ~/backup
-scp ${i}@$ip:~/ton-keys/*.seed.csv ~/backup
-done
