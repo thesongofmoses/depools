ffc1f304fc2055fc211849dcbd31bd9ac347b86a
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-19 07:34:04 +0000

    Update deployConfig.sh

--- deployConfig.sh
+++ deployConfig.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+username=$(whoami)
+hostname=$(hostname -s)
+
+deploy_addr=$(cat ~/ton-keys/$hostname.addr)
+proxy0_addr=$(cat ~/ton-keys/$hostname.proxy0.addr)
+proxy1_addr=$(cat ~/ton-keys/$hostname.proxy1.addr)
+depool_addr=$(cat ~/ton-keys/$hostname.depool.addr)
+helper_addr=$(cat ~/ton-keys/$hostname.helper.addr)
+
+cd ~/net.ton.dev/tonos-cli/target/release
 ./tonos-cli call $helper_addr \
         initTimer '{"timer":"0:325e835960b83108ed594e395167c967bfc0ede9e7ef057aae364b1c0ab75467","period":360}' \
         --abi ~/net.ton.dev/ton-labs-contracts/solidity/depool/DePoolHelper.abi.json \
