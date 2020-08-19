b78577343fa2a1efd98077f5badf1c7ed8fcb047
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-19 09:11:23 +0000

    Update stakeOrdinary.sh

--- stakeOrdinary.sh
+++ stakeOrdinary.sh
@@ -6,6 +6,8 @@ then
         exit 0
 fi
 
+if [ "${1}" == 'ordinary' ];
+then
 cd ~/net.ton.dev/tonos-cli/target/release
 ./tonos-cli depool stake ordinary --value "${1}" --autoresume-off | grep transId | awk '{print $2}' | tr -d '"' > ~/ton-keys/ordinary.confirm.txid
 
@@ -16,3 +18,5 @@ txid=$(cat ~/ton-keys/ordinary.confirm.txid)
         confirmTransaction "{"\"transactionId"\":"\"$txid"\"}" \
         --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
         --sign ~/ton-keys/$hostname.2.keys.json
+fi
+
