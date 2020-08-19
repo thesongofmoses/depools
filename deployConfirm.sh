926b57cac275c7f0a806795e28490b8b0da33100
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-19 00:08:09 +0000

    Update deployConfirm.sh

--- deployConfirm.sh
+++ deployConfirm.sh
@@ -3,13 +3,15 @@
 username=$(whoami)
 hostname=$(hostname -s)
 
+cd ~/net.ton.dev/tonos-cli/target/release
 n=$(cat ~/ton-keys/deploy.confirm.txid | wc -l)
+deploy_addr=$(cat ~/ton-keys/$hostname.addr)
 
-for (( i = 1; i <= n; i++ ));
+for (( i = 1; i <= n; i++ ))
 do
 txid=$(cat ~/ton-keys/deploy.confirm.txid | awk "FNR == ${i}")
 ./tonos-cli call $deploy_addr \
-        confirmTransaction '{"transactionId":"$txid"}' \
+        confirmTransaction "{"\"transactionId"\":"\"$txid"\"}" \
         --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json \
-        --sign ~/ton-keys/$hostname.2.keys.json 
+        --sign ~/ton-keys/$hostname.2.keys.json
 done
