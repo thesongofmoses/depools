8edbe1ddadc29f16136a17daebb0c1d90d573485
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-20 07:15:47 -0700

    Update backupWallet.sh

--- backupWallet.sh
+++ backupWallet.sh
@@ -4,8 +4,16 @@ n=$(cat ~/ip.csv | wc -l)
 for (( i = 1; i <= n; i++ ))
 
 do
+rm -rf ~/keys
+mkdir keys
+password=''
+
 ip=$(cat ~/ip.csv | awk "FNR == ${i}")
-scp username@$ip:~/ton-keys/*.keys.json ~/keys 
-scp username@$ip:~/ton-keys/*.addr ~/keys      
-scp username@$ip:~/ton-keys/*.seed.csv ~/keys      
+scp username@$ip:~/ton-keys/*.keys.json ~/keys
+echo 'yes'
+echo $password 
+scp username@$ip:~/ton-keys/*.addr ~/keys  
+echo $password
+scp username@$ip:~/ton-keys/*.seed.csv ~/keys
+echo $password
 done
